6c9436262905461e22a2c522ccf1cdcfd8239928
Nathaniel <nfgolding@gmail.com>
2020-12-21 19:48:23 +0100

    cleaning up

--- SRE_project/project/skull_basher_td/architecture/PathFinder.cpp
+++ SRE_project/project/skull_basher_td/architecture/PathFinder.cpp
@@ -7,9 +7,8 @@
 PathFinder::PathFinder(GameObject* gameObject)
  : Component(gameObject)
 {
-    PathFinder::pfMoveSpeed = 0.1f;
-    // auto currentScene = gameObject->getScene();
-    // currentPathIndex = currentScene->gameManager->getFirstPathIndex();
+    PathFinder::pfMoveSpeed = 0.1f; //not sure if the declaration in the .hpp file is working
+
     currentPathIndex = gameObject->getScene()->gameManager->getFirstPathIndex();
     // std::cout << "I am a skull, my current path index is: " << currentPathIndex << "\n";
     fetchNextPathPoint();
@@ -25,12 +24,6 @@ void PathFinder::fetchNextPathPoint(){
     auto currentScene = gameObject->getScene()->gameManager;
     
     nextPathPoint = currentScene->getNextPathPoint(currentPathIndex);
-    // auto thing = currentScene->gameManager->getNextPathPoint(currentPathIndex);
-
-    //get the next path point
-    // auto currentGameObject = this->getGameObject();
-    // auto thing = this->gameObject->getName();
-    // nextPathPoint = this->getGameObject()->getScene()->gameManager->getNextPathPoint(currentPathIndex);
 
     //update current path index
     if (currentPathIndex > 0)
@@ -62,27 +55,14 @@ void PathFinder::update(float deltaTime){
     // std::cout << "I am a skull, I should be moving to: " << nextPathPoint.x << "\n";
     //move skull
     //mix currentposition with next path point and delta time
-    //update transform
-    // auto t = glm::smoothstep(0.0f, keyframe->timeDuration, currentAnimation.second->getCurrentKeyframeTime());
- 
-    // nextPosition.x = glm::mix(nextPathPoint.x, currentPosition.x, 0.01f);
-    // // nextPosition.y = glm::mix(nextPathPoint.y, currentPosition.y, 0.01f);
-    // nextPosition.z = glm::mix(nextPathPoint.z, currentPosition.z, 0.01f);
     nextPosition.x = glm::mix(currentPosition.x, nextPathPoint.x, pfMoveSpeed); //speed is controlled with the float
     // nextPosition.y = glm::mix(currentPosition.y, nextPathPoint.y, 0.01f);
     nextPosition.z = glm::mix(currentPosition.z, nextPathPoint.z, pfMoveSpeed);
-    // nextPosition = glm::mix(currentPosition, nextPathPoint, 0.01f);
-
-    // nextPosition.x = glm::smoothstep(nextPathPoint.x, currentPosition.x, 0.1f);
-    // nextPosition.y = glm::smoothstep(nextPathPoint.y, currentPosition.y, 0.1f);
-    // nextPosition.z = glm::smoothstep(nextPathPoint.z, currentPosition.z, 0.1f);
-    // nextPosition.x = glm::smoothstep(currentPosition.x, nextPathPoint.x, 0.1f);
-    // nextPosition.y = glm::smoothstep(currentPosition.y, nextPathPoint.y, 0.1f);
-    // nextPosition.z = glm::smoothstep(currentPosition.z, nextPathPoint.z, 0.1f);
 
     // nextPosition = glm::mix(currentPosition, nextPathPoint, velocity);
     nextPosition.y = 0; // correction for the path being on the floor
-    std::cout << "I am a skull, I should be moving to: " << nextPosition.x << "\n";
+    // std::cout << "I am a skull, I should be moving to: " << nextPosition.x << "\n";
 
+    //update transform
     currentTransform->position = nextPosition;
 }
