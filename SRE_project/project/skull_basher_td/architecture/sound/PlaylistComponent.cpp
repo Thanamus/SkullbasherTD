d9498dc2bcab0c517e149ea8c1c25b2efe725cb4
Nathaniel <nfgolding@gmail.com>
2021-02-08 21:21:07 +0100

    comments on playlist comp

--- SRE_project/project/skull_basher_td/architecture/sound/PlaylistComponent.cpp
+++ SRE_project/project/skull_basher_td/architecture/sound/PlaylistComponent.cpp
@@ -11,24 +11,24 @@ PlaylistComponent::PlaylistComponent(GameObject *gameObject) : Component(gameObj
 }
 
 void PlaylistComponent::addSoundEffect(std::string soundEffectCode, std::string soundEffectName){
-    // playlist.insert(soundEffectCode, soundEffectName); // This is causing an illegal indirectin error
-    playlist[soundEffectCode] = soundEffectName;
+
+    playlist[soundEffectCode] = soundEffectName; // add a sound effect name for a given sound effect code
 }
 
-// bool PlaylistComponent::getSoundEffectName(std::string soundEffectCode, std::weak_ptr<std::string> const &nameToReturn){
+
 bool PlaylistComponent::getSoundEffectName(std::string soundEffectCode, std::string &nameToReturn){
-// bool PlaylistComponent::getSoundEffectName(std::string soundEffectCode, std::string nameToReturn){
 
+    // check if sound code + name pair exist in the map
     std::map<std::string, std::string>::iterator itr;
-
     itr = playlist.find(soundEffectCode);
 
     if (itr != playlist.end())
     {
-        // TODO check if this can be a weak ptr instead
+        // code found, pass refernce back to caller
         nameToReturn = playlist[soundEffectCode];
         return true;
     } else {
+        // sound effect code not found, output error
         std::cout << "ERROR: soundEffect: " << soundEffectCode << " for gameObject: " << gameObject->getName() <<
         " requested, but one was not found" << std::endl;        
         // *nameToReturn = nullptr;
