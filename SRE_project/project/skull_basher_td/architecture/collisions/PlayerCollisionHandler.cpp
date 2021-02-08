3b2ac62132763a79fff39db19b8cb50fa1c0a2e3
Nathaniel <nfgolding@gmail.com>
2021-02-08 21:09:19 +0100

    commented PLayer collision handler and projectile

--- SRE_project/project/skull_basher_td/architecture/collisions/PlayerCollisionHandler.cpp
+++ SRE_project/project/skull_basher_td/architecture/collisions/PlayerCollisionHandler.cpp
@@ -5,6 +5,8 @@
 #include "../GameObject.hpp"
 #include "../Component.hpp"
 #include "../Transform.hpp"
+
+
 #include "../physics/RigidBody.hpp"
 
 #include "../PersonController.hpp"
@@ -19,12 +21,15 @@
 #include "../WorldObject.hpp"
 #include "../../GameManager.hpp"
 
-class GameObject;
+class GameObject; // needed for referencing the game object
 class Component;
 
+/*
+    PlayerCollisionHandler handles collisions that happen on the PersonController (when correctly attached)
+*/
+
 PlayerCollisionHandler::PlayerCollisionHandler(GameObject* gameObject) : Component(gameObject) {
-    // gameObject->getComponent<Transform>();
-    // isGrounded = gameObject->getComponent<PersonController>()->isGrounded; // shold link PersonController and isGrounded
+
     start_footstep_lockout = std::chrono::steady_clock::now(); // initalise the first footstep lockout
 }
 
@@ -32,19 +37,28 @@ PlayerCollisionHandler::PlayerCollisionHandler(GameObject* gameObject) : Compone
 void PlayerCollisionHandler::onCollision(size_t collisionId, GameObject* other, glm::vec3 col_position, bool begin){
     if (begin){
 //        std::cout << "Collision "<< collisionId <<" on "<< other->getName() << " at "<<glm::to_string(col_position)<<std::endl;
+
+
+        // check if the other object is a world object
         if(other->getComponent<WorldObject>())
         {
-            std::string otherObjectName = other->getName();
-            glm::vec3 playerPosition = gameObject->getComponent<Transform>()->position;
-            bool& isGrounded = gameObject->getComponent<PersonController>()->isGrounded; // shold link PersonController and isGrounded
-            //std::cout << "Collision "<< collisionId <<" on "<< otherObjectName << " at "<<glm::to_string(col_position)<<std::endl;
-//            std::cout << "player position is: " << playerPosition.x << " , " << playerPosition.y << " , " << playerPosition.z << std::endl;
 
+            // other object is a world object, get the objects name
+            std::string otherObjectName = other->getName();
+            glm::vec3 playerPosition = gameObject->getComponent<Transform>()->position; // TODO check can be removed
+            bool& isGrounded = gameObject->getComponent<PersonController>()->isGrounded; // check if grounded
+            
+            // debugging couts
+            // std::cout << "Collision "<< collisionId <<" on "<< otherObjectName << " at "<<glm::to_string(col_position)<<std::endl;
+            // std::cout << "player position is: " << playerPosition.x << " , " << playerPosition.y << " , " << playerPosition.z << std::endl;
+
+            // get the SourceManager for playing a sound
             SourceManager * mySource = SourceManager::Get();
+
             //----- if colliding with ground, play ground sounds
             if (col_position.y < playerPosition.y-0.2f)
             {
-                // collision happened below the player
+                // collision happened below the player (i.e. is likely the ground)
 
                 // check footstep time
                 std::chrono::steady_clock::time_point time_now = std::chrono::steady_clock::now();
@@ -53,13 +67,13 @@ void PlayerCollisionHandler::onCollision(size_t collisionId, GameObject* other, 
                 isGrounded = true; //player just collided with something on it's feet
                 // so it could be considered grounded
 
-                // TODO match offset to player colision size
+                
                 if (time_elapsed_milli > footstep_lockout_millisec)
                 {
                     if (otherObjectName == "GrassBlock01D.obj")
                     {
                         mySource->playMyJam_global("gassy-footstep1.wav");
-                        // std::cout << "playing grassy footstep" << std::endl;
+
                     } else if (otherObjectName == "Floor01.obj"){
                         mySource->playMyJam_global("stepwood_2.wav");
                     } else if (otherObjectName == "PathBlock01D.obj"){
@@ -67,6 +81,7 @@ void PlayerCollisionHandler::onCollision(size_t collisionId, GameObject* other, 
                     } else if (otherObjectName == "Bridge01D.obj"){
 
                     }
+                    
                     // restart footstep lockout
                     start_footstep_lockout = std::chrono::steady_clock::now();
                 }
