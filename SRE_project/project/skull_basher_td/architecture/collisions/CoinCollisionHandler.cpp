2b0d75a6a57ea7b8ad5a1d13cc817f2485939c7f
Nathaniel <nfgolding@gmail.com>
2021-02-09 09:46:05 +0100

    coin collision has some comments

--- SRE_project/project/skull_basher_td/architecture/collisions/CoinCollisionHandler.cpp
+++ SRE_project/project/skull_basher_td/architecture/collisions/CoinCollisionHandler.cpp
@@ -17,25 +17,30 @@
 
 class GameObject;
 
+/*
+    coin collision handler processes collisions on the coin game objects
+*/
+
 CoinCollisionHandler::CoinCollisionHandler(GameObject *gameObject) : Component(gameObject) {}
 
 void CoinCollisionHandler::onCollision(size_t collisionId, GameObject* other, glm::vec3 position, bool begin) {
     if(gameObject->isQueuedForDeletion() || stopCollisions)
-        return;
+        return; // check if the object is going to be deleted (means a collision has already happened and the player shouldn't get more money)
 
     if (begin ) {
-
+        
+        // if the other object is the person controller
         auto player = other->getComponent<PersonController>();
         if(player)
         {
-            stopCollisions = true;
+            stopCollisions = true; // so the player only gets money once (even if there are mulitple contact points registered in bullet)
             gameObject->setQueuedForDeletion(true);
-            GameManager::getInstance().addScore(gameObject->getComponent<CoinComponent>()->getMoney());
-            SourceManager::Get()->playMyJam_global("coin.wav");
+            GameManager::getInstance().addScore(gameObject->getComponent<CoinComponent>()->getMoney()); // pay the player
+            SourceManager::Get()->playMyJam_global("coin.wav"); // play the coin sound
         }
     }
 }
 
 void CoinCollisionHandler::onCollisionEnd(size_t collisionId, GameObject *other) {
-//    std::cout << "arrow collision end!" << std::endl;
+
 }
