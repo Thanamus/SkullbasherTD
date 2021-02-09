8b7f9953255b50470eea4679e2a97959e9ccbf88
Nathaniel <nfgolding@gmail.com>
2021-02-09 12:56:07 +0100

    removed includes from projectile collision handler

--- SRE_project/project/skull_basher_td/architecture/collisions/ProjectileCollisionHandler.cpp
+++ SRE_project/project/skull_basher_td/architecture/collisions/ProjectileCollisionHandler.cpp
@@ -1,18 +1,14 @@
 
 
 #include "../GameObject.hpp"
-#include "../physics/RigidBody.hpp"
+
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/string_cast.hpp"
 #include "ProjectileCollisionHandler.hpp"
-// #include "../health/CrystalHealth.hpp"
 
-// Sound Effects _Include
-#include "../sound/SourceManager.hpp"
-#include "../Transform.hpp"
 #include "../EnemyComponent.hpp"
-#include "../health/CrystalHealth.hpp"
+
 
 class GameObject;
 
