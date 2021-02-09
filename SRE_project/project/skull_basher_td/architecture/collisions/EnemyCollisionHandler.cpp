b21f94108b5219acf95ae4ba21606776dc6532c6
Nathaniel <nfgolding@gmail.com>
2021-02-09 10:38:39 +0100

    added some comments to enemy collision handler

--- SRE_project/project/skull_basher_td/architecture/collisions/EnemyCollisionHandler.cpp
+++ SRE_project/project/skull_basher_td/architecture/collisions/EnemyCollisionHandler.cpp
@@ -12,20 +12,28 @@
 
 class GameObject;
 
+
+
 EnemyCollisionHandler::EnemyCollisionHandler(GameObject* gameObject) : Component(gameObject) {
 
 }
 
 void EnemyCollisionHandler::onCollision(size_t collisionId, GameObject* other, glm::vec3 position, bool begin) {
+    
+    //check the collision is the 'first time two obejcts collide' 
     if (begin && !stopCollisions){
         auto crystal = other->getComponent<CrystalHealth>();
 //        std::cout << "crystal " << crystal <<std::endl;
         if(crystal != nullptr)
-        {
-            stopCollisions = true;
-            gameObject->setQueuedForDeletion(true);
+        { // if other object is the crystal
+            stopCollisions = true; // shouldn't handle more collisions with the crystal
+            gameObject->setQueuedForDeletion(true); // skull should 'die' on collision with the crystal
             std::cout << "hit crystal "<<std::endl;
+
+            // decrease crystal health
             crystal->decreaseHealth(getGameObject()->getComponent<EnemyComponent>()->getDamage());
+            
+            // decrease the count of total enemies
             GameManager::getInstance().setTotalEnemies(GameManager::getInstance().getTotalEnemies() - 1);
             if(GameManager::getInstance().getTotalEnemies() <= 0)
             {
--- SRE_project/project/skull_basher_td/architecture/collisions/EnemyCollisionHandler.hpp
+++ SRE_project/project/skull_basher_td/architecture/collisions/EnemyCollisionHandler.hpp
@@ -9,9 +9,12 @@ class CollisionHandler;
 class GameObject;
 class RigidBody;
 
+/*
+ Collision handler that gets attached to 'enemy' game objects
+*/
+
 class EnemyCollisionHandler : public Component, public CollisionHandler {
 public:
-    // explicit CustomCollisionHandler(GameObject* gameObject) : Component(gameObject));
     explicit EnemyCollisionHandler(GameObject* gameObject);
 
     void onCollision(size_t collisionId, GameObject* other, glm::vec3 position, bool begin) override;
