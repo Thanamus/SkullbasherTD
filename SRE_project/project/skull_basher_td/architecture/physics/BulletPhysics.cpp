ca075aec7a8ed9d606bcb9c8bbc5e7add955c184
Nathaniel <nfgolding@gmail.com>
2021-02-09 09:56:17 +0100

    bullet physics has come comments

--- SRE_project/project/skull_basher_td/architecture/physics/BulletPhysics.cpp
+++ SRE_project/project/skull_basher_td/architecture/physics/BulletPhysics.cpp
@@ -15,6 +15,7 @@ btGhostObject* m_ghostObject = NULL;							// simple aabb ghost object (keeps tr
 btPairCachingGhostObject* m_pairCachingGhostObject=NULL;
 // "Portable" method; prerequisite call: m_dynamicsWorld->getBroadphase()->getOverlappingPairCache()->setInternalGhostPairCallback(m_ghostPairCallback);
 
+// struct for collecting the collision point info from the dynamic world callback
 struct CollisionId {
     size_t collisionId;
     GameObject* bodyA;
@@ -37,6 +38,7 @@ namespace {
  }
 
 // based on https://github.com/kripken/ammo.js/issues/82#issuecomment-426429129
+// a collision point was registered and returned to SkullBasherTD via the callback
 bool contactUpdatedCallback(btManifoldPoint& cp,void* body0,void* body1){
     bool collisionBegin = cp.m_userPersistentData == nullptr;
     auto btRigidBody0 = static_cast<btRigidBody *>(body0);
@@ -45,33 +47,32 @@ bool contactUpdatedCallback(btManifoldPoint& cp,void* body0,void* body1){
     auto rigidBody1 = static_cast<RigidBody *>(btRigidBody1->getUserPointer());
 
     if (collisionBegin){
+        // collision just began - might be able to improve performance by using this as a filter
         static size_t collisionId = 0;
         collisionId++;
-//        auto name0 = rigidBody0->getGameObject()->getName();
-//        std::cout << "collision on: " << name0 << std::endl;
-//        auto name1 = rigidBody1->getGameObject()->getName();
-//        std::cout << "collision on: " << name1 << std::endl;
         cp.m_userPersistentData = new CollisionId(collisionId, rigidBody0->getGameObject(), rigidBody1->getGameObject()); //original
     }
     auto* id = (CollisionId*)cp.m_userPersistentData;
+
+    // get the actual world position of the contact between the two bodies
     glm::vec3 pointOnA (cp.getPositionWorldOnA().x(), cp.getPositionWorldOnA().y(), cp.getPositionWorldOnA().z());
     glm::vec3 pointOnB (cp.getPositionWorldOnB().x(), cp.getPositionWorldOnB().y(), cp.getPositionWorldOnB().z());
 
-    // for (auto ph : body0GameObject->getCollisionHandlers()){
+    // trigger the individual game objects collision handlers
     for (auto ph : rigidBody0->getGameObject()->getCollisionHandlers()){
-        // ph->onCollision(id->collisionId, body1GameObject, pointOnA, collisionBegin); // original
         ph->onCollision(id->collisionId, rigidBody1->getGameObject(), pointOnA, collisionBegin);
     }
     for (auto ph : rigidBody1->getGameObject()->getCollisionHandlers()){
-    // for (auto ph : body1GameObject->getCollisionHandlers()){
         ph->onCollision(id->collisionId, rigidBody0->getGameObject(), pointOnB, collisionBegin);
-        // ph->onCollision(id->collisionId, body0GameObject, pointOnB, collisionBegin);
     }
     return true;
 }
 
+// a collision has ended callback
 bool contactDestroyedCallback(void * data) {
-    auto* id = (CollisionId*)data;
+    auto* id = (CollisionId*)data; // cast the data back to a CollisionID struct for use
+   
+    // check the bodies still exist
     if (sceneContains(id->bodyA)){
         for (auto ph : id->bodyA->getCollisionHandlers()){
             ph->onCollisionEnd(id->collisionId, id->bodyB);
@@ -92,9 +93,7 @@ bool contactDestroyedCallback(void * data) {
 BulletPhysics::BulletPhysics() {
     gContactProcessedCallback = contactUpdatedCallback;
     gContactDestroyedCallback = contactDestroyedCallback;
-    // gContactDestroyedCallback = contactDestroyedCallback;
 
-    // gMotorPreTickCallback = motorPreTickCallback;
 
     broadphase = new btDbvtBroadphase();
     collisionConfiguration = new btDefaultCollisionConfiguration();
@@ -105,11 +104,10 @@ BulletPhysics::BulletPhysics() {
 
     setGravity(gravity);
 
+//--------- for Ghost object collisions
     // from: https://pybullet.org/Bullet/phpBB3/viewtopic.php?t=6195
     m_ghostPairCallback = new btGhostPairCallback(); 
 	world->getBroadphase()->getOverlappingPairCache()->setInternalGhostPairCallback(m_ghostPairCallback);	
-    // based on https://pybullet.org/Bullet/phpBB3/viewtopic.php?t=7468
-    // world->setInternalTickCallback(motorPreTickCallback,this,true);
 
 }
 
@@ -125,11 +123,13 @@ const glm::vec3 &BulletPhysics::getGravity() const {
     return gravity;
 }
 
+// change the global gravity settings of the physics world
 void BulletPhysics::setGravity(const glm::vec3 &gravity) {
     BulletPhysics::gravity = gravity;
     world->setGravity(btVector3(gravity.x, gravity.y, gravity.z));
 }
 
+// step the physics simulation (process physics)
 void BulletPhysics::step(Scene* scene_) {
     scene = scene_;
     world->stepSimulation(timeStep, maxSubSteps);
@@ -145,6 +145,7 @@ void BulletPhysics::debugDraw(sre::RenderPass &renderPass) {
     debugDrawObj.render(renderPass);
 }
 
+// raycasting callback
 bool BulletPhysics::RaycastWorld(const glm::vec3 &Start, glm::vec3 &End) {
 
     btCollisionWorld::ClosestRayResultCallback RayCallback(btVector3(Start.x, Start.y, Start.z), btVector3(End.x, End.y, End.z));
